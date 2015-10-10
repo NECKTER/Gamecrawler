1559a94a608c6da2c5913adce30be9fe3f01c535
NECKTER <nick.scheele@yahoo.com>
2015-10-09 17:02:25 -0700

    basics

---
+++ src/SpriteSheet.java
@@ -0,0 +1,16 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class SpriteSheet {
+	private BufferedImage main;
+
+	public SpriteSheet() {
+		try {
+			main = ImageIO.read(SpriteSheet.class.getResourceAsStream("images/"));
+		} catch (IOException e) {
+		}
+	}
+}
