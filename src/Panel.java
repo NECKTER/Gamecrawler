0cafd5870354122aa17ec6b6738859a94a639de0
NECKTER <nick.scheele@yahoo.com>
2015-10-23 17:40:05 -0700

    stuff

--- src/Panel.java
+++ src/Panel.java
@@ -2,6 +2,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.AbstractAction;
 import javax.swing.JPanel;
@@ -13,6 +14,7 @@ public class Panel extends JPanel implements ActionListener {
 	private ClipPlayer sound = new ClipPlayer();
 	private SpriteSheet sheet = new SpriteSheet();
 	private MapSheet map = new MapSheet();
+	private ArrayList<Character> pressedKeys = new ArrayList<>();
 
 	//private Items item = new Items(0, 0, 0, 0, null, "main");
 
@@ -42,7 +44,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if(!pressedKeys.contains(new Character(' ')))pressedKeys.add(new Character(' '));
+				System.out.println(pressedKeys);
 			}
 		});
 		this.getActionMap().put("shootOff", new AbstractAction() {
@@ -53,7 +56,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if (pressedKeys.contains(new Character(' '))) pressedKeys.remove(new Character(' '));
+				System.out.println(pressedKeys);
 			}
 		});
 		this.getActionMap().put("Up", new AbstractAction() {
@@ -64,7 +68,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if(!pressedKeys.contains(new Character('W')))pressedKeys.add(new Character('W'));
+				System.out.println(pressedKeys);
 			}
 		});
 		this.getActionMap().put("UpOff", new AbstractAction() {
@@ -75,7 +80,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if (pressedKeys.contains(new Character('W'))) pressedKeys.remove(new Character('W'));
+				System.out.println(pressedKeys);
 			}
 		});
 		this.getActionMap().put("Left", new AbstractAction() {
@@ -86,7 +92,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if(!pressedKeys.contains(new Character('A')))pressedKeys.add(new Character('A'));
+				System.out.println(pressedKeys);
 			}
 		});
 		this.getActionMap().put("LeftOff", new AbstractAction() {
@@ -97,7 +104,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if (pressedKeys.contains(new Character('A'))) pressedKeys.remove(new Character('A'));
+				System.out.println(pressedKeys);
 			}
 		});
 		this.getActionMap().put("Down", new AbstractAction() {
@@ -108,7 +116,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if(!pressedKeys.contains(new Character('S')))pressedKeys.add(new Character('S'));
+				System.out.println(pressedKeys);
 			}
 		});
 		this.getActionMap().put("DownOff", new AbstractAction() {
@@ -119,7 +128,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if (pressedKeys.contains(new Character('S'))) pressedKeys.remove(new Character('S'));
+				System.out.println(pressedKeys);
 			}
 		});
 		this.getActionMap().put("Right", new AbstractAction() {
@@ -130,7 +140,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if(!pressedKeys.contains(new Character('D')))pressedKeys.add(new Character('D'));
+				System.out.println(pressedKeys);
 			}
 		});
 		this.getActionMap().put("RightOff", new AbstractAction() {
@@ -141,7 +152,8 @@ public class Panel extends JPanel implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				if (pressedKeys.contains(new Character('D'))) pressedKeys.remove(new Character('D'));
+				System.out.println(pressedKeys);
 			}
 		});
 	}
@@ -169,9 +181,14 @@ public class Panel extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 		// do stuff and then repaint
 
+		update();
 		repaint();
 		//System.out.println(item.getName());
 		//item.randomName();
+	}
+
+	private void update() {
+		// TODO Auto-generated method stub
 	}
 
 	@Override
@@ -179,6 +196,5 @@ public class Panel extends JPanel implements ActionListener {
 		// TODO Auto-generated method stub
 		setPreferredSize(d);
 		repaint();
-		System.out.println("resized " + getWidth() + " " + getHeight());
 	}
 }
